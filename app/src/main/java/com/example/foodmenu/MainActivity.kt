6e8bf97ccfff56f7ff1683190066ce9a1fa9cb41
julesdeborle <jules.deborle@student.hogent.be>
2022-08-17 16:44:41 +0200

    NavigationController

--- app/src/main/java/com/example/foodmenu/MainActivity.kt
+++ app/src/main/java/com/example/foodmenu/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.foodmenu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        val navController = Navigation.findNavController(this, R.id.main_fragment)
+
+        NavigationUI.setupWithNavController(bottomNavigation, navController)
     }
 }
