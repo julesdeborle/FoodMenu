3534f3cabd75e1bf9a753643d50c2fcfa894ca71
julesdeborle <jules.deborle@student.hogent.be>
2022-08-19 15:57:09 +0200

    Categories naar categoriesFragment

--- app/src/main/java/com/example/foodmenu/fragments/CategoriesFragment.kt
+++ app/src/main/java/com/example/foodmenu/fragments/CategoriesFragment.kt
@@ -1,5 +1,6 @@
 package com.example.foodmenu.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,7 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
-import com.example.foodmenu.R
+import com.example.foodmenu.activities.CategoryMealsActivity
 import com.example.foodmenu.activities.MainActivity
 import com.example.foodmenu.adapters.CategoriesAdapter
 import com.example.foodmenu.databinding.FragmentCategoriesBinding
@@ -40,6 +41,33 @@ class CategoriesFragment : Fragment() {
 
         prepareRecyclerView()
         observeCategories()
+
+        prepareCategoriesRecyclerView()
+        homeViewModel.getCategories()
+        observeCategoriesLiveData()
+        onCategoryClick()
+    }
+
+    private fun observeCategoriesLiveData() {
+        homeViewModel.observeCategoriesLiveData().observe(viewLifecycleOwner, Observer{
+                categories -> categoriesAdapter.setCategoryList(categories)
+        })
+    }
+
+    private fun prepareCategoriesRecyclerView() {
+        categoriesAdapter = CategoriesAdapter()
+        binding.recViewCategories.apply {
+            layoutManager = GridLayoutManager(context, 3, GridLayoutManager.VERTICAL, false)
+            adapter = categoriesAdapter
+        }
+    }
+
+    private fun onCategoryClick() {
+        categoriesAdapter.onItemClick = { category ->
+            val intent = Intent(activity, CategoryMealsActivity::class.java)
+            intent.putExtra(HomeFragment.CATEGORY_NAME, category.strCategory)
+            startActivity(intent)
+        }
     }
 
     private fun observeCategories() {
